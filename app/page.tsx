74dd17bd0217ff3321ddea16568c80c1c7ebe855
Ugur Cayoglu <cayoglu@me.com>
2024-05-09 15:01:11 +0200

    Add custom css files to webpage

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import styles from '@/app/ui/home.module.css';
+<div className={styles.shape} />
 
 export default function Page() {
   return (
