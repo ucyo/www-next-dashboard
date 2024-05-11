56b9bb4e20eb09e0ec27ef0613742a7ca5016c5e
Ugur Cayoglu <cayoglu@me.com>
2024-05-11 12:16:58 +0200

    Mutating Data (Edit invoices)
    - New route for editing invoices with id in route (saved within brackets)
    - Dynamic routes with data can be used with bracket style folders
    - Functions can have predefined default values using bind and mapped
      to const

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -13,6 +13,7 @@ const FormSchema = z.object({
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
     const { customerId, amount, status } = CreateInvoice.parse({
@@ -33,4 +34,23 @@ export async function createInvoice(formData: FormData) {
     `;
     revalidatePath('/dashboard/invoices');  // remove cached entry on server side for invoices table
     redirect('/dashboard/invoices')
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+    const { customerId, amount, status } = UpdateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+
+    const amountInCents = amount * 100;
+
+    await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
 }
